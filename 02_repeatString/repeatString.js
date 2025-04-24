ee1c82df5ac606349621769cef0fe0aed744730c
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-04-24 16:07:40 -0400

    02_repeatString my solution added a loop to run only if number is more than 0 if less than 0 and if 0, then added outputs to pass tests #3 and #5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+   let result = ''; 
+     if(num > 0){    
+         for(i=1 ; i <= num ; i++){
+             result = result +string     
+        }
+          return result
+    }else if(num === 0 ){
+        return result
+    }else
+     return 'ERROR';
+     };
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
