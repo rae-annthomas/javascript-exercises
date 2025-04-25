f9a46fc38cee8c46d83bec85e74fd79e1a1a2b72
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-04-25 16:15:44 -0400

    03_reverseString my solution split the given string and created a loop to start at the end of the string and add to a new variable

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+ let stringSplit= string.split("") // h e l l o
+  let len = stringSplit.length  // 5  
+  let newReverseString = '';
+ for(i = len-1 ; i >= 0 ; i--){    // [i=4]  
+    newReverseString += stringSplit[i]   
+ } 
+ return newReverseString
 };
 
 // Do not edit below this line
