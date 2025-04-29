91d58539ab651c1782ffca9ebb05bf7c2468fbba
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-04-29 13:46:52 -0400

    My solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,...rest) { //rest parameter
+  let newArray = [];
+  array.forEach((element) => {
+      if(!rest.includes(element)){
+      newArray.push(element)
+      }
+   })
+  return newArray
+}
+
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray; //[1,2,3,4],3
+
