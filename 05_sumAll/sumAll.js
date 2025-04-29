aa50039eebec2a7fdbc4f2dad4bfd283bdc2d11c
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-04-29 14:30:44 -0400

    My solution to 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
+   while (Number.isInteger(num1) && Number.isInteger(num2)){
+     if (num1 > 0 && num2 > 0){
+       let higherNum = Math.max(num1,num2);
+       let lowerNum= Math.min(num1,num2);
+       for(i=lowerNum ; i<= higherNum ; i++){
+        total += i
+       }
+       return total;
+     } else 
+        return 'ERROR'
 
-};
+   }
+   return 'ERROR'
+}
+   
+
+
+   
+
 
 // Do not edit below this line
 module.exports = sumAll;
