8263dc093123c75b3248ab95d174b7c1cb5f5399
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-04-29 18:06:00 -0400

    My solution to 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    let c = 0;
+    c = (fahrenheit - 32) * 5/9
+    if (!(c % 1 == 0)){
+       c= Number(c.toFixed(1));
+       return c
+    }else 
+      return c
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = 0;
+  f= (c * 9/5 ) + 32 
+  if (!(f % 1 == 0)){
+    f= Number(f.toFixed(1));
+       return f
+  }else 
+    return f
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
-};
+}
