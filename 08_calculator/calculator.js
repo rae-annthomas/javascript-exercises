1ed0bb057f3e55792c934a7530aa1e676d06b22a
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-06-03 11:07:59 -0400

    Completed Calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let sum = a+b
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	let subtract =  a-b
+  return subtract
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = arr.reduce((acc , currentValue) => acc + currentValue, 0)
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+ let product = arr.reduce((acc, num) => acc * num)
+ return product
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let result = Math.pow(a,b)
+  return result;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num){ // num=4  , 1 2 3 4
+  let product = 1;
+  for (let i = 2; i<=num; i++){
+    product *= i
+    }
+    return product
+  }
 
 // Do not edit below this line
 module.exports = {
