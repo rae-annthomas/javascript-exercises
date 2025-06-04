04442cd26cf9555cb2e203004972dbff4c57b1d9
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-06-04 08:19:44 -0400

    Completed Palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) { //hello World!
+  let newStr = str.toLowerCase().replace(/[^\w]/g, "").split("")
+  let strReverse = newStr.slice().reverse()
+  let j = 0
+    for (let i=0; i<=newStr.length ; i++){
+       if (newStr[i] === strReverse[j]){
+     }else{
+    return false
+    }  
+    j++
+}
+return true
+}
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
