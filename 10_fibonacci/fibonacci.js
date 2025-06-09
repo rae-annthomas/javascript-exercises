db0e62894f7ab55d690a9b52983236a10351b2c6
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-06-08 21:16:34 -0400

    Fibonacci challenge complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num = Number(n)
+if (num >= 0){
+    let rootFive = Math.sqrt(5)
+    let formulaPart1 = (1 + rootFive) / 2 
+    let a = Math.pow(formulaPart1 , num)
+
+    let formulaPart2 = (1 - rootFive) / 2 
+    let b = Math.pow(formulaPart2 , num)
+
+    let fullFormula = (1/rootFive) * (a - b)
+    return Math.floor(fullFormula)
+}else{
+   return "OOPS"
+}
+
 
 };
 
