915f5843a6bb6e4b97bb1fd15799d3851fb8cc35
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-06-08 21:47:25 -0400

    getTheTitles challenge complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+ let titleArr =[]
+    for(let i=0; i< arr.length; i++){
+        let title = arr[i].title
+        titleArr.push(title)
+    }
+    return titleArr
 };
 
 // Do not edit below this line
