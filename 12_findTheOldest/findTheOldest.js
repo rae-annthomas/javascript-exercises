9d840b0a02401247a574491de0d7dd45ff478741
rae-annthomas <76818061+rae-annthomas@users.noreply.github.com>
2025-06-12 11:18:03 -0400

    Solution to findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.map(person =>{
+        let age = 0
+        if(person.yearOfDeath === undefined){
+            const date = new Date();
+            let presentYear = date.getFullYear()
+             person.age= presentYear - person.yearOfBirth
+        } else{
+            person.age= person.yearOfDeath - person.yearOfBirth
+        }
+   })
+        let maxAge = arr.reduce((acc, current) => (current.age > acc ? current.age : acc), -                Infinity)
+        const result = arr.find(obj => obj.age === maxAge);
+          return result
 
-};
+    }
 
 // Do not edit below this line
 module.exports = findTheOldest;
